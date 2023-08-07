03f07537be2e77aed27a25c46bfe21dbf50f8f29
Andrew Toups <mistertoups@gmail.com>
2023-08-07 17:47:20 -0500

    Unalias variables.

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const list = [
 
 const listItems = list.map(item => {
   const c = IsJs.newComponent()
-  const { template: html } = c;
-  html`<li>${item}</li>`;
+  const { template } = c;
+  template`<li>${item}</li>`;
   return c;
 });
 
 const justKidding = (() => {
   const c = IsJs.newComponent();
-  const { template: html } = c;
-  html`<p>Just kidding haha</p>`;
+  const { template } = c;
+  template`<p>Just kidding haha</p>`;
   return c;
 })();
 
@@ -27,22 +27,22 @@ const badMood = app.state(true);
 const imSuchAGeminiLol = app.state(false);
 const orDoI = () => { imSuchAGeminiLol.set(true) };
 badMood.onSet(({newVal}) => {if (newVal === true) imSuchAGeminiLol.set(false)});
-const { is: js, template: html } = app;
-html`
-<div class='app grid-center' data-class=${js`[${badMood} === true ? 'bad-mood' : 'good-mood', ${imSuchAGeminiLol} && 'forgiving']`}>
+const { is, template } = app;
+template`
+<div class='app grid-center' data-class=${is`[${badMood} === true ? 'bad-mood' : 'good-mood', ${imSuchAGeminiLol} && 'forgiving']`}>
   <div class='card'>
-    <h2>Frameworks I ${js`${badMood} === true ? 'hate' : 'love'`}!</h2>
+    <h2>Frameworks I ${is`${badMood} === true ? 'hate' : 'love'`}!</h2>
     <ul>
       <div data-list=${listItems}></div>
     </ul>
-    <div data-if=${js`${badMood} === true && ${imSuchAGeminiLol} === true`}>
+    <div data-if=${is`${badMood} === true && ${imSuchAGeminiLol} === true`}>
       <div data-component=${justKidding}></div>
     </div>
-    <div data-if=${js`${badMood} === true && ${imSuchAGeminiLol} === false`}>
+    <div data-if=${is`${badMood} === true && ${imSuchAGeminiLol} === false`}>
       <p>And I really mean it!!</p>
     </div>
     <button data-click=${badMood.toggle}>Toggle Mood</button>
-    <div data-if=${js`${imSuchAGeminiLol} === false && ${badMood} === true`}>
+    <div data-if=${is`${imSuchAGeminiLol} === false && ${badMood} === true`}>
       <button data-click=${orDoI}>...or do I??</button>
     </div>
   </div>
